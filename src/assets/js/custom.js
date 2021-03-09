7cabd636f3928804e98a7d82c4c0b20dd34f1ef1
AaronTheNerd <bargeaaron@gmail.com>
2021-03-09 13:02:41 -0500

    generated deployment

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,6 +1,7 @@
 $(function() {
     $("#projects-carousel").on('slide.bs.carousel', function(e) {
         var id = $(e.relatedTarget).index();
+        console.log(id);
         $('#indicators > li > a.active').removeClass('active');
         $('#indicators > li > a[data-slide-to=' + id + ']').addClass('active');
     });
